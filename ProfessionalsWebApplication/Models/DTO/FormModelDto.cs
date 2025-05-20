69a54a5fd54da423f471203aebb3b9b6e4ee568b
c3n9 <108518693+c3n9@users.noreply.github.com>
2025-05-20 20:39:56 +0300

    обновление FormModelDto и контроллера

--- ProfessionalsWebApplication/Models/DTO/FormModelDto.cs
+++ ProfessionalsWebApplication/Models/DTO/FormModelDto.cs
@@ -7,9 +7,6 @@ public class FormModelDto
     [Required(ErrorMessage = "Название формы обязательно")]
     public string Name { get; set; } = string.Empty;
 
-    [Required(ErrorMessage = "Видимость формы обязательна")]
-    public bool IsVisible { get; set; } = true;
-
     [Required(ErrorMessage = "Дата начала обязательна")]
     public DateTime DateStart { get; set; }
 
